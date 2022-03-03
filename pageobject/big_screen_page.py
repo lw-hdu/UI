c1f26dd07f9f2e21ee84d085a67d19eec1d2cbd4
lw-hdu <915077145@qq.com>
2022-03-03 11:16:27 +0800

    添加日志

--- pageobject/big_screen_page.py
+++ pageobject/big_screen_page.py
@@ -2,18 +2,23 @@
 Descripttion: 
 Author: Liuwen
 Date: 2021-12-10 10:35:56
-LastEditTime: 2022-03-03 10:05:28
+LastEditTime: 2022-03-03 11:03:19
 '''
 import time
 from selenium.webdriver.common.by import By
 from base.base_page import BasePage
+import logging
 
 class BigScreenPage(BasePage):
+
+    logging.basicConfig(level=logging.INFO)
+    log = logging.getLogger()
 
     page_screen_loc = (By.XPATH,'//li[@class="el-menu-item"]/span[text()="数据大屏"]')
 
     def big_screen(self):
 
         #进入大屏
-        self.click(BigScreenPage.page_screen_loc)
+        self.click(self.page_screen_loc)
+        self.log.info(f'定位的元素为{self.page_screen_loc}')
         time.sleep(2)
--- testcase/test_big_screen.py
+++ testcase/test_big_screen.py
@@ -2,22 +2,15 @@
 Descripttion: 
 Author: Liuwen
 Date: 2021-12-10 10:45:15
-LastEditTime: 2022-03-03 09:37:36
+LastEditTime: 2022-03-03 11:02:10
 '''
-from selenium import webdriver
+
 from pageobject.login_page import LoginPage
 from pageobject.big_screen_page import BigScreenPage
 from base.base_util import BaseUtil
 
 
 class TestBigScreen(BaseUtil):
-    # def setup_class(self):
-    #     self.driver = webdriver.Chrome()
-    #     print('-------start big screen-------')
-    
-    # def teardown_class(self):
-    #     print('-------end big screen-------')
-    #     self.driver.quit()
 
         
     def test_bigscreen(self):
